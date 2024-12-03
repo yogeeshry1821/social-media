42fc8d407eaf0d39456fd7f80dfc57372c10023e
Yogeesh <yryadappa100@gmail.com>
2024-12-03 17:07:45 +0530

    changes2

--- client/src/scenes/loginpage/Form.jsx
+++ client/src/scenes/loginpage/Form.jsx
@@ -23,7 +23,7 @@ const registerSchema = yup.object().shape({
   password: yup.string().required("required"),
   location: yup.string(),
   occupation: yup.string(),
-  picture: yup.string().required("required"),
+  picture: yup.string(),
 });
 const loginSchema = yup.object().shape({
   email: yup.string().email("invalid email").required("required"),
